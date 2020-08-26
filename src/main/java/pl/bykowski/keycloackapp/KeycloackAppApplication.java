61c98e931eacf730cfe7a9f3f3342a2faa9a41ba
Przemek Bykowski <przemek@bykowski.pl>
2020-08-26 16:04:24 +0200

    Initial commit

---
+++ src/main/java/pl/bykowski/keycloackapp/KeycloackAppApplication.java
@@ -0,0 +1,13 @@
+package pl.bykowski.keycloackapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloackAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KeycloackAppApplication.class, args);
+  }
+
+}
---
+++ src/test/java/pl/bykowski/keycloackapp/KeycloackAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bykowski.keycloackapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloackAppApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
